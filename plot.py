9d112653e3350df5bb485b1e406356480265117e
Atsushi Ishikawa <ishikawasuteado@gmail.com>
2020-06-11 12:12:14 +0900

    improved plot

--- plot.py
+++ plot.py
@@ -15,18 +15,25 @@ df2 = df2.set_index("unique_id")
 df = pd.concat([df1, df2], axis=1)
 df = df[df["reaction_energy"].notna()]
 
-values = []
-color = ["grey", "blue", "green"]
-for _, irow in df.iterrows():
-    value = irow["reaction_energy"]
-    run = irow["run"]
-    values.append([value,color[run]])
+runmax = df["run"].max()
+runmin = df["run"].min()
 
-values = sorted(values, key=lambda x: x[0])
+df3 = df.sort_values("reaction_energy")
+df3 = df3.reset_index()
 
-value = list(map(lambda x: x[0], values))
-color = list(map(lambda x: x[1], values))
 
-plt.bar(range(len(value)), value, color=color, alpha=0.4)
-plt.ylim([np.min(value)-0.05, np.min(value)+0.1])
+def norm(run):
+    return (run - runmin) / (runmax - runmin)
+
+
+plt.figure(figsize=(12,6))
+for i in range(runmax+1):
+    current = df3[df3["run"]==i]
+    pos = current.index.values
+    val = current.reaction_energy
+    color = plt.cm.Blues(norm(i))
+    label = "run %2d" % i
+    plt.bar(pos, val, color=color, label=label)
+plt.legend()
 plt.show()
+
