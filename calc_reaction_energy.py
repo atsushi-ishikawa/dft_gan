5ac57e638e37139f71f94b21cbbf005d4ae5a984
ishi <ishikawasuteado@gmail.com>
2020-06-16 19:12:20 +0900

    added vasp

--- calc_reaction_energy.py
+++ calc_reaction_energy.py
@@ -1,15 +1,19 @@
 from ase import Atoms
 from ase.build import add_adsorbate
 from ase.visualize import view
+from ase.calculators.vasp import Vasp
 from ase.calculators.emt import EMT
 from ase.db import connect
 from ase.optimize import BFGS
 import json
 import os
 import pandas as pd
+import numpy as np
 
 surf_json = "surf.json"
 reac_json = "reaction_energy.json"
+
+calculator = "vasp"
 
 # argvs   = sys.argv
 # injson  = argvs[1] # json for surfaces
@@ -27,16 +31,40 @@ else:
     df_reac = df_reac.set_index("unique_id")
 
 db1 = connect(surf_json)
-calc = EMT()
+
+if "vasp" in calculator:
+    prec = "low"
+    xc   = "pbe"
+    nsw  = 0
+    ibrion = -1
+    kpts = [1, 1, 1]
+    pp   = "potpaw_PBE.54"
+
+    calc = Vasp(label=None, prec=prec, xc=xc, ibrion=ibrion ,nsw=nsw, kpts=kpts, pp=pp)
+else:
+    calc = EMT()
 
 numdata = db1.count() + 1
 
 check = False
+
+def set_unitcell(Atoms, vacuum=10.0):
+	import numpy as np
+	cell = np.array([1, 1, 1])*vacuum
+	Atoms.set_cell(cell)
+
+def set_calculator_with_label(Atoms, calc):
+    name = Atoms.get_chemical_formula()
+    if "vasp" in calculator:
+        print("set calculator to ", name)
+        calc.set_label(name)
+    Atoms.set_calculator(calc)
 #
 # reactant
 #
 reac = Atoms("N2", [(0, 0, 0), (0, 0, 1.1)])
-reac.set_calculator(calc)
+set_unitcell(reac)
+set_calculator_with_label(reac, calc)
 print(" --- calculating %s ---" % reac.get_chemical_formula())
 opt = BFGS(reac)
 opt.run(fmax=0.1)
@@ -65,7 +93,8 @@ for id in range(1, numdata):
         # not found -- calculate here
         #
         print(" --- calculating %s ---" % surf.get_chemical_formula())
-        surf.set_calculator(calc)
+        set_calculator_with_label(surf, calc)
+        #surf.set_calculator(calc)
         opt = BFGS(surf)
         opt.run(fmax=0.1)
         Esurf = surf.get_potential_energy()
@@ -78,7 +107,7 @@ for id in range(1, numdata):
         Eprod_surf = surf.get_potential_energy()
 
         Ereactant = Esurf + Ereac
-        Eproduct = Eprod_surf
+        Eproduct  = Eprod_surf
         deltaE = Eproduct - Ereactant
         print("deltaE = %5.3e, Ereac = %5.3e, Eprod = %5.3e" % (deltaE, Ereactant, Eproduct))
 
