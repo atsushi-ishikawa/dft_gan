2deaabc7e5824bebe5928246db9ab09831de64fd
ISHIKAWA Atsushi <ishikawasuteado@gmail.com>
2021-05-06 14:12:17 +0900

    from mio

--- sub2.sh
+++ sub2.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+calculators=whisky
+dir=/home/a_ishi/ase/nn_reac/
+
+echo "$calculators:$dir"
+
+# copy json file from VASP calculating clusters
+scp $calculators:$dir/{surf,reaction_energy}.json ./
+
 python nn_reac.py
 python make_todo_list.py
 
+scp ./{surf.json,todolist.txt} $calculators:$dir
+
+# give back surf.json to calculating clusters
+#scp whisky:/home/a_ishi/ase/nn_reac/{surf,reaction_energy}.json ./
