30bcb65bd3eba18277e996631829d956b2fb0108
ishi <ishikawasuteado@gmail.com>
2021-05-06 15:03:55 +0900

    from whisky

--- run_whisky.sh
+++ run_whisky.sh
@@ -3,7 +3,9 @@
 #$ -S /bin/bash
 #$ -cwd
 #$ -q all.q
-#$ -pe openmpi12 12
+#$ -pe openmpi24 24 
 #$ -l hostname=whisky1?
 
-python calc_reaction_energy.py --calculator vasp 1> stdout$$.txt 2> stderr$$.txt 
+ID=$1
+
+python calc_reaction_energy.py --id $ID --calculator vasp 1> stdout$$.txt 2> stderr$$.txt 
