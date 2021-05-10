9e319d5e03f6a30273b5b35eabb55d33bfedae52
ishi <ishikawasuteado@gmail.com>
2021-05-10 14:16:58 +0900

    from whisky

---
+++ sub0.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+### preparation 
+surf_json="surf.json"
+numdata=$1
+todolist="todolist.txt"
+
+# cleanup
+rm $todolist reaction_energy.json loss.h5 2> /dev/null
+rm -rf ./log 2> /dev/null
+
+python make_surf.py $numdata
+python make_todo_list.py
+
